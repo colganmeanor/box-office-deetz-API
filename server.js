ae4589cba42f37684aa678f6be47c29fdb4b7c58
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-11 18:48:36 -0700

    Refactored to just request an id from the post request, refactored parseInt to Number and added a 1 parameter to the splice method

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ app.locals.favoriteMovies = []
 
 app.get('/api/v1/favorite-movies', (request, response) => {
   const faves = app.locals.favoriteMovies;
-
   response.json({ faves });
 });
 
 app.get('/api/v1/favorite-movies/:id', (request, response) => {
-    const id = request.params.id;
-    const foundFave = app.locals.favoriteMovies.find(movie => movie.id === parseInt(id))
+  const id = request.params.id;
+  const foundFave = app.locals.favoriteMovies.find(movie => movie.id === Number(id))
 
   response.status(200).json(foundFave)
 });
@@ -23,32 +22,25 @@ app.get('/api/v1/favorite-movies/:id', (request, response) => {
 app.post('/api/v1/favorite-movies', (request, response) => {
   const fave = request.body;
 
-    for (let requiredParameter of ['id', 'poster_path', 'backdrop_path', 'title', 'average_rating', "release_date"]) {
-    if (!fave[requiredParameter]) {
-      response
-        .status(422)
-        .send({ error: `Expected format: { id: <Number>, poster_path: <String>, backdrop_path: <String>, title: <String>, average_rating: <String>, release_date: <String> }. You're missing a "${requiredParameter}" property.` });
-    }
+  if (!fave.id) {
+    response
+      .status(422)
+      .send({ error: `Id is required.` });
   }
 
-  const { id, poster_path, backdrop_path, title, average_rating, release_date } = fave
-  app.locals.favoriteMovies.push({ id, poster_path, backdrop_path, title, average_rating, release_date });
-  response.status(201).json({ id, poster_path, backdrop_path, title, average_rating, release_date });
-  console.log(app.locals.favoriteMovies)
+  app.locals.favoriteMovies.push(fave.id);
+  response.status(201).json({ id: fave.id });
+  console.log(app.locals.favoriteMovies);
 });
 
 app.delete('/api/v1/favorite-movies/:id', (request, response) => {
   const id = request.params.id
-  const foundFave = app.locals.favoriteMovies.find(movie => movie.id === parseInt(id))
+  const foundFave = app.locals.favoriteMovies.find(movie => movie.id === Number(id))
 
-  app.locals.favoriteMovies.splice(foundFave)
+  app.locals.favoriteMovies.splice(foundFave, 1)
   console.log(app.locals.favoriteMovies)
   response.send('DELETED')
-
-  
 });
-
-
 
 app.listen(app.get('port'), () => {
   console.log(`Box Office Deetz API is running on http://localhost:${app.get('port')}.`);
